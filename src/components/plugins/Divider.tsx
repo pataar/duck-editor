ba112884daf01f118cdb5027de36062f4f55daac
pataar <pietering1@gmail.com>
2018-08-17 16:30:33 +0200

    Add divider component

---
+++ src/components/plugins/Divider.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { DuckPluginProps } from "./IDuckPluginProps";
+
+class Divider extends React.Component<DuckPluginProps, {}> {
+	constructor(props: any) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="duck-toolbar-divider" />
+		);
+	}
+}
+
+export default Divider;
